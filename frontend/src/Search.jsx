55db4607ca600cdc6514375999c1c867af5e3bca
Simran Sharma <4simransharma@gmail.com>
2023-07-31 21:32:47 +0530

    added footerr

--- frontend/src/Search.jsx
+++ frontend/src/Search.jsx
@@ -73,6 +73,9 @@ const Search = () => {
                 : ""//<p>Empty Result</p>
                 }
             </div>
+            <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: '#f0f0f0' }}>
+                Made with love from <a href="https://github.com/3simransharma/Recipe-Search" target="_blank" rel="noopener noreferrer">GitHub</a>
+            </footer>
         </Box>
     );
 };
